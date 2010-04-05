642e967121e1039e733584e745c321faf9e669d1
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2010-04-05 21:23:21 +0300

    Remove old plot-all-probsize.sh.

--- plot-all-probsize.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh -G
-rm -rf plots/probsize/*
-cat runs.dat | awk '{print $1}' | xargs -n1 basename | sed 's/\.[^.]*$//' | xargs -n1 ./plot-probsize.sh | fgrep .eps... | cut '-d ' -f7 | sed 's/...$//' | while read -r f; do mv -v $f plots/probsize; done
